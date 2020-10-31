e688200fb401cb9b0fa6be9fce8f2671dbd4c3d9
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-10-31 21:14:55 +0530

    refactored code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const { jwtKey } = process.env;
+const httpStatus = require('http-status-codes').StatusCodes;
 
 const Authenticate = (req, res, next) => {
   jwt.verify(req.headers.authorization, jwtKey, (err, decoded) => {
     if (err) {
-      res.status(401).json({ data: null, errors: err.message, code: 401 });
+      res.status(httpStatus.UNAUTHORIZED).json(
+        { data: null, errors: err.message, code: httpStatus.UNAUTHORIZED },
+      );
     } else {
       req.user = decoded.email;
       next();

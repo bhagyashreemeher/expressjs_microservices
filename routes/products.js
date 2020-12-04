57d0d7bee4683f676b7f07854f8ec796552c3104
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-12-04 22:52:12 +0530

    replaced hardcoded string with status codes

--- routes/products.js
+++ routes/products.js
@@ -34,27 +34,27 @@ router.get('/:id', authenticate, (req, res) => {
 router.get('/', authenticate, (req, res) => {
   Product.find({}, (err, doc) => {
     if (!err) {
-      res.status(200).json({ data: doc, errors: null, code: 200 });
+      res.status(httpStatus.OK).json({ data: doc, errors: null, code: httpStatus.OK });
     } else {
-      res.status(400).json({ data: null, errors: err.message, code: 400 });
+      res.status(httpStatus.BAD_REQUEST).json({ data: null, errors: err.message, code: httpStatus.BAD_REQUEST });
     }
   });
 });
 
 router.delete('/:id', authenticate, (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id) || !req.params.id || req.params.id === '') {
-    res.status(400).json({ error: `The user with product id ${req.params.id} not found` });
+    res.status(httpStatus.BAD_REQUEST).json({ error: `The user with product id ${req.params.id} not found` });
     return;
   }
 
   Product.findByIdAndDelete(req.params.id, (err, doc) => {
     if (!err) {
       if (!doc) {
-        res.status(404).json({ error: `The user with product id ${req.params.id} not found` });
+        res.status(httpStatus.NOT_FOUND).json({ error: `The user with product id ${req.params.id} not found` });
       } else {
-        res.status(200).json({ data: doc, errors: null, code: 200 });
+        res.status(httpStatus.OK).json({ data: doc, errors: null, code: httpStatus.OK });
       }
-    } else res.status(400).json({ data: null, errors: err.message, code: 400 });
+    } else res.status(httpStatus.BAD_REQUEST).json({ data: null, errors: err.message, code: httpStatus.BAD_REQUEST });
   });
 });
 

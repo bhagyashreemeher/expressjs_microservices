6f4c69ebb8d2f9c49ec84e6f4169c142d56f2f6d
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-12-08 23:35:44 +0530

    minor changes

--- routes/orders.js
+++ routes/orders.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const Order = require('../models/order');
 const mongoose = require('../database/db');
 const authenticate = require('../middleware/auth');
+const httpStatus = require("http-status-codes").StatusCodes;
 
 router.post('/', authenticate, (req, res) => {
   const orders = [];
@@ -17,9 +18,9 @@ router.post('/', authenticate, (req, res) => {
 
   Order.create(orders, (err, doc) => {
     if (!err) {
-      res.status(200).json({ data: doc, errors: null, code: 200 });
+      res.status(httpStatus.OK).json({ data: doc, errors: null, code: httpStatus.OK });
     } else {
-      res.status(400).json({ data: null, errors: err.message, code: 400 });
+      res.status(httpStatus.BAD_REQUEST).json({ data: null, errors: err.message, code: httpStatus.BAD_REQUEST });
     }
   });
 });
@@ -33,11 +34,11 @@ router.get('/:id', authenticate, (req, res) => {
   Order.findById(req.params.id, (err, doc) => {
     if (!err) {
       if (!doc) {
-        res.status(404).json({ error: `The user with order id ${req.params.id} not found` });
+        res.status(httpStatus.NOT_FOUND).json({ error: `The user with order id ${req.params.id} not found` });
       } else {
-        res.status(200).json({ data: doc, errors: null, code: 200 });
+        res.status(httpStatus.OK).json({ data: doc, errors: null, code: httpStatus.OK });
       }
-    } else res.status(400).json({ data: null, errors: err.message, code: 400 });
+    } else res.status(httpStatus.BAD_REQUEST).json({ data: null, errors: err.message, code: httpStatus.BAD_REQUEST });
   });
 });
 
